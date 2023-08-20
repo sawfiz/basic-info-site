10eb2324f136a4243b0a004feed7e132900c9f3e
sawfiz <dashu.up@gmail.com>
2023-08-20 18:33:56 -0400

    Optimization

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Import the core modules
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
@@ -7,34 +8,33 @@ const path = require('path');
 const hostname = 'localhost';
 const port = 8080;
 
+// The 404 page
+const page404Path = path.join(__dirname, 'public', '404.html');
+const page404 = fs.readFileSync(page404Path, (err, data) => {
+  if (err) throw err;
+  return data;
+});
+
 // Create a server instance using the http module
 const server = http.createServer((req, res) => {
+  const q = url.parse(req.url, true);
 
   // Routing the URL request
   let fileName;
-  switch (req.url) {
-    case '/':
-      fileName = 'index.html';
-      break;
-    case '/about':
-      fileName = 'about.html';
-      break;
-    case '/contact':
-      fileName = 'contact.html';
-      break;
-
-    default:
-      fileName = '404.html';
-      break;
+  if (q.pathname === '/') {
+    fileName = 'index.html';
+  } else {
+    // remove the '/'
+    fileName = q.pathname.slice(1) + '.html';
   }
   const filePath = path.join(__dirname, 'public', fileName);
 
   // Read the HTML file and serve
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Internal Server Error');
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(page404);
     } else {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');

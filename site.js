83f3f76446e16519be514df2f750b9a04756a432
sawfiz <dashu.up@gmail.com>
2023-08-20 16:08:50 -0400

    Add serving index.html

--- site.js
+++ site.js
@@ -1,18 +1,36 @@
-// Import the http module
+// Import the core modules
 const http = require('http');
+const fs = require('fs')
+const path = require('path')
 
 // Define the hostname and port number
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 8080;
 
 // Create a server instance using the http module
 const server = http.createServer((req, res) => {
-  // Set the response header
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  // Check if the requested URL is the root path
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'public', 'index.html')
+    // Read the index.html file
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('Internal Server Error')
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html')
+        res.end(data)
+      }
+    })
+  } else {
+    // Handle other URLs
+    res.statusCode = 404;
+    res.setHeader = ('Content-Type', 'text/plain')
+    res.end('404 Not Found')
+  }
 
-  // Send response message
-  res.end('Hello World\n');
 });
 
 // Start the server and listen on the specified host name and port
